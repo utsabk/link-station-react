063580f2cb34477b8593711dbb915b561a3fead0
utsabk <kharelutsab@gmail.com>
2021-10-14 21:48:15 +0300

    Fix updaing data only one time issue

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import StatioForm from './components/StationForm';
 import Station from './components/Station';
 import Calculate from './components/Calculate';
 import Response from './components/Response';
-import './App.css'
+import './App.css';
 
 const App = () => {
   const [points, setPoints] = useState([]);
@@ -52,8 +52,6 @@ const App = () => {
 
   const handleCalculation = () => {
 
-    setResponses([]);
-
     const distanceCalculator = (pointA, pointB) => {
       const base = pointA[0] - pointB[0];
       const height = pointA[1] - pointB[1];
@@ -72,7 +70,9 @@ const App = () => {
       }
     };
 
-    console.log('Points', points);
+    // use local copy of array beacuse calling setState directly inside a loop only updates one time
+    const reponsesCopy = [];
+
     points.forEach((points) => {
       let previousPower = 0;
       let bestStation = [0, 0];
@@ -86,25 +86,21 @@ const App = () => {
       });
 
       if (previousPower === 0) {
-        setResponses(
-          responses.concat(
-            `No link station within reach for point ${points[0]}, ${points[1]}`
-          )
+        reponsesCopy.push(
+          `No link station within reach for point ${points[0]}, ${points[1]}`
         );
       } else {
-        setResponses(
-          responses.concat(
-            `Best link station for point ${points[0]},${points[1]} is ${bestStation[0]},${bestStation[1]} with power ${previousPower}`
-          )
+        reponsesCopy.push(
+          `Best link station for point ${points[0]},${points[1]} is ${bestStation[0]},${bestStation[1]} with power ${previousPower}`
         );
       }
     });
+
+    setResponses(responses.concat(reponsesCopy));
   };
 
   return (
     <div className="container">
-      {console.log('Response:-', responses)}
-
       <Header />
       <div className="point-box">
         {points.length === 0 && <p>Add new point</p>}
